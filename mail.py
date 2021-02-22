7edb0db985b2582e81ed1347f6accf1a37e40b7f
Doug Mumme <dougmumme@gmail.com>
2021-02-21 19:10:03 -0600

    added the ability to send attachments

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+import pathlib
 import smtplib
 from email.message import EmailMessage
 
@@ -7,6 +8,15 @@ msg['From'] = 'dpmumme12@gmail.com'
 msg['To'] = 'dpmumme12@gmail.com'
 msg.set_content('That Works!')
 
+# Replace file name with file you wish to send
+with open('test.pdf', 'rb') as f:
+    file_data = f.read()
+    file_type = pathlib.Path(f.name).suffix
+    file_name = f.name
+
+
+msg.add_attachment(file_data, maintype = 'application', subtype = file_type, filename = file_name)
+    
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login('dpmumme12@gmail.com', 'heciuecbuvjisaeg')
 
